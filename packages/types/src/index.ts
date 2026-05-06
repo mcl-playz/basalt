c73496e37c020a559d44b97d56c50036e4ab6f1f
Jasper Green <99476285+mcl-playz@users.noreply.github.com>
2026-05-06 08:48:52 +0800

    feat(types): add shared mailbox and message types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,24 @@
+export type MessageKey = {
+    mailbox: string;
+	uid: number;
+}
+
+export interface MessageMetadata extends MessageKey {
+    sender: string;
+	subject: string;
+	date: Date;
+    read: boolean;
+}
+
+export interface MessageBody extends MessageKey {
+    text?: string;
+    html?: string;
+}
+
+export interface Message extends MessageMetadata, MessageBody {}
+
+export interface Mailbox {
+    path: string;
+    name: string;
+    delimiter: string;
+}
