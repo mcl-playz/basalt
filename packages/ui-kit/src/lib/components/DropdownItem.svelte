de04cae8c2be1ac1f0b4df17643b11569e8eb11b
Jasper Green <99476285+mcl-playz@users.noreply.github.com>
2026-05-21 21:35:45 +0800

    feat(ui-kit): make DropdownItem support being used as a SubTrigger

--- packages/ui-kit/src/lib/components/DropdownItem.svelte
+++ packages/ui-kit/src/lib/components/DropdownItem.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
 import { DropdownMenu } from "bits-ui";
-import type { Component } from "svelte";
+import type { Component, } from "svelte";
 import type { MouseEventHandler } from "svelte/elements";
+import { CaretRightIcon } from "phosphor-svelte";
 
 let {
 	icon: Icon,
 	title,
+    subtrigger = false,
 	onclick,
 }: {
 	icon: Component;
 	title: string;
-	onclick?: MouseEventHandler<HTMLDivElement>;
+    subtrigger?: boolean;
+	onclick?: MouseEventHandler<HTMLButtonElement | HTMLDivElement>;
 } = $props();
+
+// svelte-ignore state_referenced_locally
+const Element = subtrigger ? DropdownMenu.SubTrigger : DropdownMenu.Item;
 </script>
 
-<DropdownMenu.Item class="flex items-center gap-1.5" {onclick}>
+<Element class="flex w-full items-center gap-1.5" {onclick}>
     <Icon />
-    {title}
-</DropdownMenu.Item>
+    <span>{title}</span>
+
+    {#if subtrigger}
+        <CaretRightIcon class="ml-auto size-3" />
+    {/if}
+</Element>
