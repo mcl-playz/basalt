6d1e1709fac15219dfcee5ca6fae3fcbc3f7d061
Jasper Green <99476285+mcl-playz@users.noreply.github.com>
2026-05-21 14:11:34 +0800

    feat(ui-kit): add DropdownItem component

---
+++ packages/ui-kit/src/lib/components/DropdownItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { DropdownMenu } from "bits-ui";
+import type { Component } from "svelte";
+import type { MouseEventHandler } from "svelte/elements";
+
+let {
+	icon: Icon,
+	title,
+	onclick,
+}: {
+	icon: Component;
+	title: string;
+	onclick?: MouseEventHandler<HTMLDivElement>;
+} = $props();
+</script>
+
+<DropdownMenu.Item class="flex items-center gap-1.5" {onclick}>
+    <Icon />
+    {title}
+</DropdownMenu.Item>
